a1f021c71acc9af307be5f50593db07451766510
Joselyn Chavez <joselynchavezf@gmail.com>
2023-11-21 10:26:13 -0500

    add terra startup scripts

---
+++ terrabio_startup_scripts/suite.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+pip install --upgrade libgsl-dev
+pip install --upgrade libglpk-dev
+pip install --upgrade libgdal-dev
+pip install --upgrade imagemagick
+pip install --upgrade libmagick++-dev
+pip install --upgrade igraph
+pip install --upgrade leidenalg
+pip install --upgrade networkx
+pip install --upgrade community
+
+RUN R -e "install.packages('BiocManager')"
+RUN R -e "BiocManager::install(version = '3.18')"
+RUN R -e "BiocManager::install('beachmat', dependencies=TRUE)"
+RUN R -e "BiocManager::install('HDF5Array', dependencies=TRUE)"
+
+R -e "install.packages('remotes', dependencies=TRUE)"
+R -e "remotes::install_github('drieslab/Giotto')"
+
+
