6750c4f3d0d3497e8213bfdc11009c26631a5e8a
JunxiangXu <50884991+JunxiangXu@users.noreply.github.com>
2024-04-27 04:04:47 -0400

    Update Supplementary_figure_8_A_B.R

--- scripts/Supplementary_figure_8_A_B.R
+++ scripts/Supplementary_figure_8_A_B.R
@@ -1,4 +1,15 @@
+## %######################################################%##
+#                                                          #
+####      Supplementary figure 8 Multi-segmentatioon        ####
+#                                                          #
+## %######################################################%##
 
+## Download data
+Original Xenium data can be downloaded from 10X: https://www.10xgenomics.com/products/xenium-in-situ/preview-dataset-human-breast
+## Registered intermediate files
+## Registered files can be downloaded from Zenodo in order to reproduce the figure: 10.5281/zenodo.11075079
+## These files include Xenium_FFPE_Human_Breast_Cancer_Rep1_he_image_compressed.png(target coordinate system), CD20_aligned.csv.gz, DAPI_aligned.csv.gz, HER2_aligned.csv.gz, cell_boundaries_aligned.csv.gz, nucleus_boundaries_aligned.csv.gz, Xenium_Rep1_STalign_to_VisiumHE.csv
+## Also, aligned multi-segmentation results can also be downloaded from Zenodo, including aligned_cellpose_v3.csv, aligned_stardist_v3.csv, Baysor_polygons_aligned.csv
 
 ############################# Preprocess and Load ##############################
 
