8358cd1d2f55e6d30d82aecccb9059f1729110ee
George Chen <72078254+jiajic@users.noreply.github.com>
2025-05-06 19:32:31 -0400

    temp_updates
    - needs further sorting

--- scripts/Supplementary_figure_6.R
+++ scripts/Supplementary_figure_6.R
@@ -1,6 +1,6 @@
 ## %##########################################################%##
 #                                                              #
-####                 Supplementary figure 6                    #
+####                 Supplementary figure 4                    #
 #                                                              #
 ## %##########################################################%##
 
@@ -23,6 +23,9 @@ instructions(g, "save_dir") <- "results"
 instructions(g, "show_plot") <- TRUE
 instructions(g, "return_plot") <- FALSE
 instructions(g, "save_plot") <- TRUE
+
+fig_dir <- file.path("scripts", "FIGURE", "S4")
+dir.create(fig_dir, showWarnings = FALSE)
 
 z_digits <- 0:6
 z_levels <- paste0("z", z_digits)
@@ -83,9 +86,11 @@ vcolors <- viridisLite::turbo(8)
 vcoltab <- data.table::data.table(sum = 0:7, color = vcolors)
 terra::coltab(poly_rs_sum) <- vcoltab
 
-##### Fig S6 B ---------------------------------------------------------- ####
+##### Fig S4 B ---------------------------------------------------------- ####
 
+svg(file.path(fig_dir, "B.svg"), height = 4, width = 4.1)
 plot(poly_rs_sum) # plot of multiple layers
+dev.off()
 
 ## rasterized feature comparisons ##
 
@@ -195,13 +200,17 @@ cell_polys <- g@spatial_info[paste0("z", 0:6)]
 cell_centroids <- lapply(cell_polys, GiottoClass::centroids)
 
 
-##### Fig S6 C ---------------------------------------------------------- ####
+##### Fig S4 C ---------------------------------------------------------- ####
 # plot the total sum across all layers
+svg(file.path(fig_dir, "C.svg"), height = 9, width = 9)
 plot(sum_all, col = mycolors, range = sum_range, main = "all layers")
 plot(g@spatial_info$aggregate, add = TRUE)
 plot(centroids(g@spatial_info$aggregate), add = TRUE, cex = 0.5)
+dev.off()
 
-# plot the total sum for each layer (figure 6C continued)
+##### Fig S4 D ---------------------------------------------------------- ####
+
+# plot the total sum for each layer (figure S4 D continued)
 vals <- list()
 for (i in 1:7) {
     sum_spat_genes_layer <- lapply(X = new_savelist, function(x) {
@@ -215,18 +224,23 @@ for (i in 1:7) {
         as.numeric()
 
     # plot
+    svg(file.path(fig_dir, sprintf("D%d.svg", i)), height = 9, width = 9)
     plot(sum_all_layer, col = mycolors, main = paste0("layer = ", i))
     plot(cell_polys[[i]], add = T)
     plot(cell_centroids[[i]], add = T, cex = 0.5)
+    dev.off()
 }
 
 
-##### Fig S6 E ---------------------------------------------------------- ####
+##### Fig S4 E ---------------------------------------------------------- ####
 
 # plot as boxplot
 vals_DF <- Reduce(cbind, vals)
 colnames(vals_DF) <- z_levels
+
+svg(file.path(fig_dir, "E.svg"), height = 9, width = 9)
 boxplot(vals_DF)
+dev.off()
 
 # COV
 cov_spat_genes <- lapply(X = new_savelist, function(x) {
@@ -249,15 +263,17 @@ plot(g@spatial_info$aggregate, type = "centroid", add = TRUE, cex = 0.5)
 
 sum_cov_all <- sum(cov_spat_genes_vector, na.rm = TRUE)
 
-##### Fig S6 F ---------------------------------------------------------- ####
+##### Fig S4 F ---------------------------------------------------------- ####
 
 # plot total cov
+svg(file.path(fig_dir, "F.svg"), height = 9, width = 9)
 plot(sum_cov_all, col = mycolors, main = "total COV")
 plot(g@spatial_info$aggregate, add = TRUE)
 plot(g@spatial_info$aggregate,
     type = "centroid",
     add = TRUE, cex = 0.5
 )
+dev.off()
 
 spatial_diff <- data.table::data.table(genes = genes, sum = unlist(sum_spat_genes), cov = unlist(cov_spat_genes))
 spatial_diff[, sumt := sum(terra::values(sum[[1]], na.rm = T)), by = 1:nrow(spatial_diff)]
@@ -268,7 +284,7 @@ spatial_diff[1:50]
 
 spatial_diff[sumt > 1500]
 
-##### Fig S6 G ---------------------------------------------------------- ####
+##### Fig S4 G ---------------------------------------------------------- ####
 
 library(ggplot2)
 pl <- ggplot()
@@ -280,25 +296,27 @@ pl
 pl <- pl + geom_point(
     data = spatial_diff[genes == "Gad1"],
     aes(
-        x = log(sumt + 1), y = log(covt + 1),
-        color = "red"
-    )
+        x = log(sumt + 1), y = log(covt + 1)
+    ),
+    color = "red"
 ) +
     geom_text(
         data = spatial_diff[genes == "Gad1"],
         aes(
-            x = log(sumt + 1), y = log(covt + 1),
-            color = "red"
+            x = log(sumt + 1), y = log(covt + 1)
         ),
+        color = "red",
         label = "Gad1",
         hjust = -0.2
     )
 pl
-
+ggsave(file.path(fig_dir, "G.svg"), plot = pl, height = 9, width = 9)
 
 plot_layers_gene(new_savelist, genes, "Cspg5")
 
-##### Fig S6 H ---------------------------------------------------------- ####
+##### Fig S4 H ---------------------------------------------------------- ####
 
 # example of gene with differences in distribution across the z slices
+svg(file.path(fig_dir, "H.svg"), height = 9, width = 9)
 plot_layers_gene(new_savelist, genes, "Gad1")
+dev.off()
